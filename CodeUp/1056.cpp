49552e6c69fdf55c2156a2c504bbcb21431693da
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-10-06 22:36:12 +0900

    10/6일 문제 풀기

---
+++ CodeUp/1056.cpp
@@ -0,0 +1,11 @@
+//CodeUp 1056
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", (!a&&b)||(a&&!b));
+
+	return 0;
+}
---
+++ CodeUp/1059.cpp
@@ -0,0 +1,11 @@
+//CodeUp 1059
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	printf("%d", ~a);
+
+	return 0;
+}
